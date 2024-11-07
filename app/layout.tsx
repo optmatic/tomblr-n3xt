2c2322ca88a6c0147d56d926977b25ed40fe7cd7
optmatic <optmatic@thomass-MacBook-Air.local>
2024-11-07 15:25:15 +1000

    yes, final 7/11

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 
 export const metadata: Metadata = {
-  title: "[Thomas Hand] - Optimising the web to the nth degree",
+  title: "Thomas Hand - Optimising the web to the nth degree",
   description: "I've never been great at writing meta descriptions.",
   icons: {
     apple: [
