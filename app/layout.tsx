1abe711fc6f50ad5ecd1106cd9ef67d2f26ef176
optmatic <optmatic@thomass-MacBook-Air.local>
2024-11-07 15:22:10 +1000

    yes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,16 @@ import { GeistMono } from 'geist/font/mono';
 export const metadata: Metadata = {
   title: "[Thomas Hand] - Optimising the web to the nth degree",
   description: "I've never been great at writing meta descriptions.",
+  icons: {
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
